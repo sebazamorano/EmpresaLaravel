f8d3b3a94a242ac90c7a10b0d146fa4c37d94af0
Sebastian Zamorano <sebazamoranoj@gmail.com>
2018-02-19 19:20:19 -0300

    cambios de proveedores

--- app/Http/Controllers/ProveedorController.php
+++ app/Http/Controllers/ProveedorController.php
@@ -9,6 +9,14 @@ use Illuminate\Validation\ValidationException;
 class ProveedorController extends Controller
 {
     /**
+     * ProveedorController constructor.
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
