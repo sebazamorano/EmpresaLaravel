dbc3117b32f81b80491f19c971b1a4befa01a7f0
Sebastian Zamorano <sebazamoranoj@gmail.com>
2018-02-19 19:36:51 -0300

    merge deni

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -9,6 +9,11 @@ use Illuminate\Validation\ValidationException;
 
 class CategoriaController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,7 +36,8 @@
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
-                        &nbsp;<li><a href="{{ route('proveedores.index') }}">Proveedores</a></li>
+                        <li><a href="{{ route('proveedores.index') }}">Proveedores</a></li>
+                        <li><a href="{{ route('categoria.index') }}">Categoria</a></li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
